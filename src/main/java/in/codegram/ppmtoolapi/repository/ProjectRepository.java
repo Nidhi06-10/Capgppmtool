76d5b2ccffd43f6416bedb9df9f6cce60b371096
Nidhi06-10 <nidhinishmitha06@gmail.com>
2020-11-25 23:09:13 -0600

    project validation done

--- src/main/java/in/codegram/ppmtoolapi/repository/ProjectRepository.java
+++ src/main/java/in/codegram/ppmtoolapi/repository/ProjectRepository.java
@@ -1,5 +1,13 @@
 package in.codegram.ppmtoolapi.repository;
 
-public interface ProjectRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import in.codegram.ppmtoolapi.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	//TODO: No need to add any CRUD method, methods can be customized
 
 }
